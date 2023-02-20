947b2cddf3b55895aae27c189ed317fc734d9120
jomiller8149 <118348391+jomiller8149@users.noreply.github.com>
2023-02-21 03:46:02 +0530

    terraform commits

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+region = "ap-south-1"
+access_key = "AKIA44OKMRHYL7ZAKHXN"
+secret_key = "5MsnylHwNyrb6WQyFotSHzholuJA0jWfQ6a5d84d"
+}
+
+resource "aws_instance" "ec2_example" {
+ami                      = "ami-0e742cca61fb65051"
+instance_type            = "t2.micro"
+tags = {
+  Name = "Terraform EC2"
+  }
+}
